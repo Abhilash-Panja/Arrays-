1f77b4673cb2f1cc99629b684fc3735c42bccee6
Abhilash Panja <abhilashpanja7@gamil.com>
2023-06-11 15:54:16 +0530

    updated code

--- Arrays/src/Arrays/Sorting_colors.java
+++ Arrays/src/Arrays/Sorting_colors.java
@@ -1,5 +1,5 @@
 package Arrays;
-
+import java.util.*;
 public class Sorting_colors {
 	static void sorting(int [] a) {
 		int temp=0;
@@ -19,11 +19,37 @@ public class Sorting_colors {
 //		a[correctposition]=temp;
 //		
 //	}
+	static void Sorting(int [] a) {
+		int low=0,high=a.length-1,mid=0;
+		while(mid<high) {
+			switch(a[mid]) {
+			case 0:{
+				swap(a,low,mid);
+				low++;
+				mid++;
+				break;}
+			case 1:{
+				mid++;
+				break;}
+			case 2:{
+				swap(a,mid,high);
+				mid++;
+				high--;
+				break;}
+			}
+			
+				
+		}
+	}
+	 static void swap(int[] a, int low, int mid) {
+		int temp=a[low];
+		a[low]=a[mid];
+		a[mid]=temp;
+		
+	}
 	public static void main(String[] args) {
 	int [] a= {2,0,2,1,1,0};
-	sorting(a);
-	for(int i: a) {
-		System.out.print(i+" ");
-	}
+	Sorting(a);
+	System.out.println(Arrays.toString(a));
 	}
 }
